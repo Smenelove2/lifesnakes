54c0a5c0d8e60d3b44e758de88d3d2d62c85b9be
Smenelove2 <felipemenelaucontato@gmail.com>
2024-11-05 23:45:23 -0300

    implementando o telas.h
    implementando o telas.h

---
+++ include/telas.h
@@ -0,0 +1,16 @@
+#ifndef TELAS_H
+#define TELAS_H
+
+#include <stdio.h> 
+#include <unistd.h>
+
+
+int tela_inicio();
+int escolha_dificuldade();
+void derrota();
+void vitoria();
+int drawMap();
+int instrucoes();
+void leaderboard();
+
+#endif
